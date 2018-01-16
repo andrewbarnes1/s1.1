f0136f6827a5fbd456136a71ef310b11211ec9ea
andrew <andrewbarneswork@outlook.com>
2018-01-16 14:31:01 +0000

    Added functionality to make the lights flash RED BLUE GREEN. Contained in functions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,47 @@
 #include <mbed.h>
 
+void on(DigitalOut colour)
+{
+  colour.write(0);
+}
+
+void off(DigitalOut colour)
+{
+  colour.write(1);
+}
+
+
 int main() {
 
-    DigitalOut red(PTB22);
+  DigitalOut red(PTB22);
+  DigitalOut blue(PTB21);
+  DigitalOut green(PTE26);
 
-    while(1) {
-      red.write(0);
-      wait(0.5);
-      red.write(1);
-      wait(0.5);
-    }
+  while(1) {
+    off(blue);
+    off(green);
+    on(red);
+    wait(0.5);
+    off(red);
+    on(blue);
+    wait(0.5);
+    off(blue);
+    on(green);
+    wait(0.5);
+  }
+
+  /*
+  while(1) {
+  blue.write(1);
+  green.write(1);
+  red.write(0);
+  wait(0.5);
+  red.write(1);
+  blue.write(0);
+  wait(0.5);
+  blue.write(1);
+  green.write(0);
+  wait(0.5);
+}
+*/
 }
